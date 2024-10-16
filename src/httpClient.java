3e6a26de8f1cbe78473629b087d938bbf11503b4
zAlexisD <alexis.doan@ensea.fr>
2024-10-16 17:55:49 +0200

    httpClient class 1.2 WIP : The returned file is empty for some reasons

--- src/httpClient.java
+++ src/httpClient.java
@@ -6,7 +6,7 @@ import java.net.URL;
 public class httpClient {
     public String protocol, host, filename;
     public int port;
-    public long sleepTime = 100;
+    public long sleepTime = 10;
 
     public httpClient(int port, String protocol, String host, String filename) {
         this.port = port;
@@ -30,15 +30,16 @@ public class httpClient {
         Socket socket = new Socket(host,port);
         Thread.sleep(sleepTime);
 
-        InputStream from = socket.getInputStream();
         OutputStream output = socket.getOutputStream();
         PrintWriter to = new PrintWriter(output);
 
-        to.println("GET "+ filename +" HTTP/1.1");
-        to.println("Host: "+ host);
-        to.println("User-Agent: Java-ClientHTTP");
+        to.println("GET "+ filename +" HTTP/1.1\r\n"+"Host: "+ host+"\r\n"+"User-Agent: XXX");
 
-        OutputStream out = new FileOutputStream(filename);
+        int lastSlashIndex = filename.lastIndexOf('/');
+        String newFileName = filename.substring(lastSlashIndex+1);
+        OutputStream out = new FileOutputStream(newFileName);
+        InputStream from = socket.getInputStream();
+
         byte[] buf = new byte[4096];
         int bytes_read;
         while((bytes_read = from.read(buf)) != -1){
